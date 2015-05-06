330df483b65ffbb6c86385b1827dd294adef94f5
adiagarwalla <aa4@princeton.edu>
2015-05-06 13:46:31 -0400

    LDA done. Need to test

--- lda.py
+++ lda.py
@@ -1,10 +1,25 @@
 import lda
 import numpy as np
 
+num_train = 8974
+num_test = 1026
+file_extension = "10k"
+number_topics = 15
+number_iterations = 100
+
+# reading a bag of words file back into python. The number and order
+# of emails should be the same as in the *samples_class* file.
+def read_bagofwords_dat(myfile, numofposts):
+    bagofwords = np.fromfile(myfile, dtype=np.uint8, count=-1, sep="")
+    bagofwords = np.reshape(bagofwords,(numofposts,-1))
+    return bagofwords
+
 # LDA on bag of words of titles across all posts
-def lda(bagOfWords, topics, iterations, vocab, trainTitles, testBagOfWords, testTitles):
+def ldaPost(bagOfWords, topics, iterations, vocab, trainTitles, testBagOfWords, testTitles):
 	model = lda.LDA(n_topics=topics, n_iter=iterations)
 	model.fit(bagOfWords)
+
+	print "Fit train bag of words and the shape of the topic-word distribution is - "
 
 	#Distribution over vocabulary for each topic
 	topic_word = model.topic_word_
@@ -12,6 +27,7 @@ def lda(bagOfWords, topics, iterations, vocab, trainTitles, testBagOfWords, test
 	#Shape of topic_word
 	print topic_word.shape
 
+	print "Top 5 vocab words per topic"
 	# Top 5 vocab words per topic
 	n = 5
 	for i, topic_dist in enumerate(topic_word):
@@ -21,9 +37,11 @@ def lda(bagOfWords, topics, iterations, vocab, trainTitles, testBagOfWords, test
 	#Distribution over topics for each post
 	post_topic = model.doc_topic_
 
+	print "Shape of trainDocument-topic distribution is - "
 	#Shape of post_topic
 	print post_topic.shape
 
+	print "Top topic for 10 posts are - "
 	# Top topic for 10 posts
 	for n in range(10):
 		topic_most_pr = post_topic[n].argmax()
@@ -34,9 +52,15 @@ def lda(bagOfWords, topics, iterations, vocab, trainTitles, testBagOfWords, test
 	#Run fit transform for held out test data
 	test_post_topic = model.fit_transform(testBagOfWords)
 
+	outfile= open("test_doc_topic.txt", 'w')
+        outfile.write(test_post_topic)
+        outfile.close()
+
+	print "Fit transformed test bag of words and shape of document - topic distribution is - "
 	#Make sure shape of this is correct
 	print test_post_topic.shape
 
+	print "Top topic for 10 posts are - "
 	#Distribution over topics for 10 posts in the test dataset
 	for n in range(10):
 		test_topic_most_pr = test_post_topic[n].argmax()
@@ -44,17 +68,35 @@ def lda(bagOfWords, topics, iterations, vocab, trainTitles, testBagOfWords, test
 
 	print "end"
 
-	#Prediction results for all the topics
-	# results = []
+def main():
+	file_train = "train_"+file_extension+"_bag_of_words.dat"
+	file_test = "test_"+file_extension+"_bag_of_words.dat"
+	train = read_bagofwords_dat(file_train, num_train)
+	test = read_bagofwords_dat(file_test, num_test)
 
-	# for i, value in enumerate(testRow):
-	# 	sumC = 0
-	# 	for k in range(15):
-	# 		sumC += doc_topic[value][k] * topic_word[k][testCol[i]]
- #        results.append(sumC)
-    
- #    print results
+	print "Read Bag of words files"
 
+	file_vocab = "train_"+file_extension+"_vocab.txt"
+	train_vocab = []
+        with open(file_vocab) as f:
+            train_vocab = f.readlines()
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+        print "Read train vocabulary file"
+
+        file_titles = "train_"+file_extension+"_samples.txt"
+        train_titles = []
+        with open(file_titles) as f:
+            train_titles = f.readlines()
+
+        print "Read train titles file"
+
+        file_test_titles = "test_"+file_extension+"_samples.txt"
+        test_titles = []
+        with open(file_test_titles) as f:
+            test_titles = f.readlines()
+
+        print "Read test titles file"
+
+        ldaPost(train, number_topics, number_iterations, train_vocab, train_titles, test, test_titles)
+
+main()
