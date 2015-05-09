6dfb487a3398352f9dfeabe3f6b5c5222be94e8f
Helen <heleny@princeton.edu>
2015-05-09 15:04:38 -0400

    decision tree

--- analyse.py
+++ analyse.py
@@ -4,26 +4,27 @@ filenames = ["svm_100k_predictions.txt", "knn_100k_predictions.txt", "ldasvm_100
 file_test_target = open("test_100k_classes.txt", "r")
 test_y = [int(l) for l in file_test_target.readlines()]
 file_test_target.close()
-results = [[0, 0 ,0],[0, 0 ,0],[0, 0 ,0]]
 
 for filename in filenames:
+	results = [[0, 0 ,0],[0, 0 ,0],[0, 0 ,0]]
 	print "---------------------"
 	print filename[:-4]
-	f = open(filename, "r")
-	predictions = [int(l) for l in f.readlines()]
+	f_prediction = open(filename, "r")
+	predictions = [int(l) for l in f_prediction.readlines()]
+	f_prediction.close()
 	count = 0
 	for i, j in zip(test_y, predictions):
 		count += 1
-		if filename[:-4] == "cosine_prediction_100k_classes":
-			if count == 750:
-				break
+		#if filename[:-4] == "cosine_prediction_100k_classes":
+			#if count == 750:
+		#		break
 		results[i][j] += 1
 	print results
 	for classification in range(0,3):
-		tp = 0
-		tn = 0
-		fp = 0
-		fn = 0
+		tp = 0.0
+		tn = 0.0
+		fp = 0.0
+		fn = 0.0
 		print "classification ", classification
 		for target in range(0, 3):
 			for prediction in range(0,3):
@@ -36,8 +37,13 @@ for filename in filenames:
 				else:
 					tn += results[target][prediction]
 
-		precision = tp/(tp + fp + 0.0)
-		recall = tp/(tp + fn + 0.0)
+		assert(tp + tn + fp + fn == len(predictions))
+
+		if tp + fp == 0:
+			precision = float("NaN")
+		else:
+			precision = tp/(tp + fp)
+		recall = tp/(tp + fn)
 		# print "fp", fp
 		# print "fn", fn
 		# print "tn", tn
