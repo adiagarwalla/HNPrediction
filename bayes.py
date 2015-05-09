9edb13e73eb7528659e38a1bb846c1cb3e467431
adiagarwalla <aa4@princeton.edu>
2015-05-09 15:25:41 -0400

    Basic bayes works

--- bayes.py
+++ bayes.py
@@ -3,7 +3,7 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords
 from os import listdir
 from os.path import isfile, isdir, join
-import numpy
+import numpy as np
 import re
 import sys
 import getopt
@@ -14,9 +14,16 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
 
+file_extension = "10k"
+if file_extension == "100k": 
+    num_train = 89961
+    num_test = 10039
+    neg_train = -89961
+else:
+    num_train = 8974
+    num_test = 1026
+    neg_train = -8974
 
-num_train = 45000
-num_test = 5000
 # reading a bag of words file back into python.
 def read_bagofwords_dat(myfile, numofposts):
     bagofwords = np.fromfile(myfile, dtype=np.uint8, count=-1, sep="")
@@ -24,52 +31,71 @@ def read_bagofwords_dat(myfile, numofposts):
     return bagofwords
 
 def main():
-    # Bag of words for training data
-    file_train = "../train_emails_bag_of_words_200.dat"
-
-    # Bag of words for test data
-    file_test = "../test_emails_bag_of_words_0.dat"
+    if file_extension == "100k":
+        file_train = "../IgnoreData/train_100k_bag_of_words.dat"
+    else:
+        file_train = "train_"+file_extension+"_bag_of_words.dat"
+    file_test = "test_"+file_extension+"_bag_of_words.dat"
     train = read_bagofwords_dat(file_train, num_train)
     test = read_bagofwords_dat(file_test, num_test)
+    print "Read Bag of words files"
 
-    train_target = []
-    for i in range(0, num_train):
-        if i < num_train/2:
-            train_target.append("notspam")
-        else:
-            train_target.append("spam")
+    file_titles = "train_"+file_extension+"_samples.txt"
+    train_titles = []
+    with open(file_titles) as f:
+        train_titles = [line.rstrip() for line in f]
 
-    test_target = []
-    for i in range(0, num_test):
-        if i < num_test/2:
-            test_target.append("notspam")
-        else:
-            test_target.append("spam")
+    print "Read train titles file"
+
+    file_test_titles = "test_"+file_extension+"_samples.txt"
+    test_titles = []
+    with open(file_test_titles) as f:
+        test_titles = [line.rstrip() for line in f]
+
+    print "Read test titles file"
+
+    file_train_classes = "train_"+file_extension+"_classes.txt"
+    train_classes = []
+    with open(file_train_classes) as f:
+        train_classes = [line.rstrip() for line in f]
+
+    file_test_classes = "test_"+file_extension+"_classes.txt"
+    test_classes = []
+    with open(file_test_classes) as f:
+        test_classes = [line.rstrip() for line in f]
+
+    print "Read test classes file whose length is"
+    print len(test_classes)
 
     cutoff = 2.5
-    class_prior = [.2, .8]
-    #nb = GaussianNB()
+    # class_prior = [.2, .8]
+    nb = GaussianNB()
     #nb = MultinomialNB(1.0, False, class_prior)
-    nb = BernoulliNB(1.0, cutoff, False, class_prior)
-    model = nb.fit(train, train_target)
+    # nb = BernoulliNB(1.0, cutoff, False, class_prior)
+    model = nb.fit(train, train_classes)
 
     y_pred = model.predict(test)
 
+    outfile= open("bayes_"+file_extension+"_classes.txt", 'w')
+    outfile.write("\n".join(y_pred))
+    outfile.close()
+
     print("Number of mislabeled test points out of a total %d points : %d" 
-          % (len(y_pred),(test_target != y_pred).sum()))
+          % (len(y_pred),(test_classes != y_pred).sum()))
 
-    vocab = []
-    with open("trec07p_data/Test/train_emails_vocab_200.txt") as f:
-        vocab = f.readlines()
+    # file_vocab = "train_"+file_extension+"_vocab.txt"
+    # train_vocab = []
+    # with open(file_vocab) as f:
+    #     train_vocab = f.readlines()
 
-    vocab = [x.strip('\n') for x in vocab]
+    # train_vocab = [x.strip('\n') for x in train_vocab]
 
-    for i in range(0, num_test):
-        if y_pred[i] != test_target[i]:
-            print y_pred[i], 
-            for j in range(0, len(test[i])):
-                if test[i][j] > cutoff: print vocab[j],
-            print "\n -----------------------"
+    # for i in range(0, num_test):
+    #     if y_pred[i] != test_classes[i]:
+    #         print y_pred[i], 
+    #         for j in range(0, len(test[i])):
+    #             if test[i][j] > cutoff: print train_vocab[j],
+    #         print "\n -----------------------"
 
 
 main()
